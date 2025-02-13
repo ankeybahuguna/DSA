0dc4973e507e36b74da6e24da6aa019b721e3657
Ankey <98467272+ankeybahuguna@users.noreply.github.com>
2025-02-13 19:38:39 +0530

    Time: 18 ms (12.30%) | Memory: 43.2 MB (74.55%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+            int left = i + 1, right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == target) return sum;
+                
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+                
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return closestSum;
+    }
+}
